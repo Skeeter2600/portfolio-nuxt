cbb129dbcb040a68afac614d1667382a3de44e6a
Skeeter2600 <andersbe2600@gmail.com>
2025-02-28 00:29:34 -0600

    maybe

--- plugins/global-css.js
+++ plugins/global-css.js
@@ -1,7 +1,7 @@
-import Vue from 'vue'
+// import Vue from 'vue'
 
-// This plugin ensures CSS is loaded properly
-export default () => {
-  // This is a no-op plugin that ensures the webpack CSS loader processes our CSS
-  Vue.prototype.$ensureCSS = true
-}
+// // This plugin ensures CSS is loaded properly
+// export default () => {
+//   // This is a no-op plugin that ensures the webpack CSS loader processes our CSS
+//   Vue.prototype.$ensureCSS = true
+// }
